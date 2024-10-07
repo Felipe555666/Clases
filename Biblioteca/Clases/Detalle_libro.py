8e34acecaaccf84dc0d5e9eb880c907d2531554e
Felipe Aguero <felipe.aguero08@inacapmail.cl>
2024-10-07 18:30:24 -0300

    VariablesPrivadas

---
+++ Biblioteca/Clases/Detalle_libro.py
@@ -0,0 +1,7 @@
+class Detalle_libro:
+    def __init__(self,ISBN,Genero,Formato,N_pag,ID_editorial):
+        self.__ISBN = ISBN
+        self.__Genero = Genero
+        self.__Formato = Formato
+        self.__N_pag = N_pag
+        self.__ID_editorial = ID_editorial
---
+++ Biblioteca/Clases/Prestamo.py
@@ -0,0 +1,9 @@
+class Prestamo:
+    def __init__(self,ISBN,Fecha_p,Fecha_e,Fecha_d,N_usuario,Estado_po):
+        self.__ISBN = ISBN
+        self.__Fecha_p = Fecha_p
+        self.__Fecha_e = Fecha_e
+        self.__Fecha_d = Fecha_d
+        self.__N_usuario = N_usuario
+        self.__Estado_po = Estado_po
+        
